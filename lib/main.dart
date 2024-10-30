cafd39db53f03cbce25bcf2e7540feab779541a0
Ziad Alaa <ziadalaa955@gmail.com>
2024-10-30 19:06:29 +0300

    create home page

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/views/home_page.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomePage.id: (context) => const HomePage(),
+      },
+      initialRoute: HomePage.id,
+    );
+  }
+}
