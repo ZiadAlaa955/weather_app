9ebaf632b438f88ee6a4d837033b051efa55baeb
Ziad Alaa <ziadalaa955@gmail.com>
2024-11-02 14:46:46 +0200

    create method and emit states

--- lib/Cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/Cubits/get_weather_cubit/get_weather_cubit.dart
@@ -1,5 +1,17 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/Cubits/get_weather_cubit/get_weather_state.dart';
+import 'package:weather_app/Models/weather_model.dart';
+import 'package:weather_app/Services/weather_service.dart';
 
 class GetWeatherCubit extends Cubit {
-  GetWeatherCubit(super.initialState);
+  GetWeatherCubit() : super(NoWeatherState());
+  getCurrentWeather({required String cityName}) async {
+    try {
+      WeatherModel weatherModel =
+          await WeatherService().getCurrentWeather(cityName: cityName);
+      emit(WeatherLoadedState());
+    } catch (e) {
+      emit(WeatherFaliureState());
+    }
+  }
 }
